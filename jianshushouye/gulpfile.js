4cd01d8a01ae8281f2310b8334a5cf3439242f94
guoshengqiu <1172744949@qq.com>
2019-08-03 22:50:09 +0800

    vue study

---
+++ jianshushouye/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+/**
+ * anonymous function - description
+ *
+ * @return {type}  description
+ */
+gulp.task('hello', function () {
+  console.log('Hello World!');
+});
+gulp.task('first', function () {
+  console.log('agdb');
+});
+
+function a(a, b) {
+  console.log(a);
+}
---
+++ my-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="app">
+    <img src="./assets/logo.png">
+    <span class= "text">abfw</span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+  color: red;
+  .text {
+    font-size: 40px;
+  }
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  // color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
